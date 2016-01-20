c44adf2aff821f48f6db308e5b50aacb1db9192d
blastoff <blastoff2@hotmail.com>
2016-01-20 11:40:12 +0000

    added questions

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -7,12 +7,12 @@ it { is_expected.to respond_to(:dock).with(1).argument }
 
 it 'docks something' do
 	bike = Bike.new
-	expect(subject.dock(bike)).to eq bike
+	expect(subject.dock(bike)).to include(bike)
 end
 
 it 'raises an error when the docking station is full' do
 	bike = Bike.new
-	subject.dock(bike)
+	20.times { subject.dock(bike) }
 	expect { subject.dock(bike) }.to raise_error 'Docking station full'
 end
 
@@ -20,6 +20,11 @@ describe '#release_bike' do
 it 'raises an error when there are no bikes available' do
 	expect{ subject.release_bike }.to raise_error 'No bikes available'
 end
+
+
+
+
+
 end
 
 end
